ca84c9f2d1279fbae6cba9106297482e3c76cdbf
benji2512 <benthedev@outlook.com>
2018-11-15 20:38:26 +0000

    Script finished

--- vimscript.sh
+++ vimscript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+cd ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/benji2512/VimSetUp.git ~/
+git clone https://github.com/benji2512/VimSetUp.git ~/VimSetUp
 cd VimSetUp/
 mv .vimrc  ~/
+cd ~/
+rm -rf ~/VimSetUp
 vim +PluginInstall +qall
