68407a3ecd46c6c54cffb49219cf9cb28cdd09c5
benji2512 <benthedev@outlook.com>
2018-11-15 21:03:04 +0000

    added echos

--- vimscript.sh
+++ vimscript.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo "Starting cloning"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/benji2512/VimSetUp.git ~/VimSetUp
+echo "finished cloning making .vimrc file"
 cd VimSetUp/
 mv .vimrc  ~/
 cd ~/
-rm -rf ~/VimSetUp
 vim +PluginInstall +qall
+echo "Finished Plugin Install"
+rm -rf ~/VimSetUp
