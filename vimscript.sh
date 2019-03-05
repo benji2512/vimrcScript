c969116c4cfe0c3abe6aef7b7f674ebd3b0138ba
Ben <benthedev@outlook.com>
2019-03-05 10:32:14 +0000

    added different os install variation

--- vimscript.sh
+++ vimscript.sh
@@ -10,3 +10,14 @@ cd ~/
 vim +PluginInstall +qall
 echo "Finished Plugin Install"
 rm -rf ~/VimSetUp
+OSNAME = $(lsb_release -is)
+if [ $(OSNAME) = 'Ubuntu' ]
+then
+    sudo apt install build-essential cmake python3-dev
+elif [ $(OSNAME) = 'openSUSE' ]
+then
+    sudo zypper install cmake
+    sudo zypper install -t pattern devel_C_C++ devel_python3
+fi
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py
