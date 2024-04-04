bde9af4f96c89217532ed64cc9dfb5a1cbef53bb
Ben Clarke <ben.clarke1@ibm.com>
2024-04-04 17:17:00 +0100

    Updated file after vimrc update

--- vimscript.sh
+++ vimscript.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 cd ~/
-echo "Starting cloning"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/benji2512/VimSetUp.git ~/VimSetUp
+echo "Installing vim-plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Starting ssh cloning"
+git clone git@github.com:benji2512/VimSetUp.git ~/VimSetUp
 echo "finished cloning making .vimrc file"
 cd VimSetUp/
 mv .vimrc  ~/
 cd ~/
+echo "Installing Plugins"
 vim +PluginInstall +qall
 echo "Finished Plugin Install"
 rm -rf ~/VimSetUp
